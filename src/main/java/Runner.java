2cfb93689c1064c1396e49e350544ec0829287c7
Adam Bennett <nyoxidetwitter@gmail.com>
2021-06-29 16:51:33 -0400

    improve map examples

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,14 @@
 import examples.*;
 
 public class Runner {
+
+    // Basic Benefits of using Collections & Inheritance
+
+    // 1. Make code more - readable
+    // 2. Make code more - reusable
+    // 3. Make code more - organized
+
+
     public static void main(String[] args) {
         //InstanceOfExample.run();
         //SortingExample.run();
--- src/main/java/examples/MapExample.java
+++ src/main/java/examples/MapExample.java
@@ -1,14 +1,43 @@
 package examples;
 
-import inheritance.*;
-
 import java.util.*;
 
+@SuppressWarnings("UnusedAssignment")
 public class MapExample {
+
+
+    // ARRAY
+
+    // [ Red, Large... ]
+
+
+    // MAP
+
+    // [
+    //  Color: Red,
+    //  Size: Large
+    // ]
 
     public static void run()
     {
-        Map<String, Automobile> archive = new HashMap<>();
+
+        var list = new ArrayList<String>();
+        list.add("Red");
+        list.add("Large");
+
+        // To get object color from list, we must memorize or hardcode list.get(0)
+        String color = list.get(0);
+
+        var test = list.contains("Red");
+
+        var map = new HashMap<String, String>();
+        map.put("Color", "Red");
+        map.put("Size", "Large");
+
+        // To get object color from the map, we just need to ask for it
+        color = map.get("Color");
+
+        System.out.println("Color: " + color);
 
     }
 }
